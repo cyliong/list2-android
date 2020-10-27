db202fa1b57860cab9d0ca5345706cb11cce7723
cyliong <liongtheprogrammer@gmail.com>
2020-10-27 23:27:00 +0800

    Lay out list view in widget

--- app/src/main/java/com/example/ltp/list2/widget/ListWidget.kt
+++ app/src/main/java/com/example/ltp/list2/widget/ListWidget.kt
@@ -35,10 +35,8 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.list_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
