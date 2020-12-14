a5f60fdad600ce7386c760c398be5d32c29c20ea
cyliong <liongtheprogrammer@gmail.com>
2020-12-14 22:31:54 +0800

    Fix "Redundant nullable return type" warning

--- app/src/main/java/com/example/ltp/list2/ui/ListItemsFragment.kt
+++ app/src/main/java/com/example/ltp/list2/ui/ListItemsFragment.kt
@@ -27,7 +27,7 @@ class ListItemsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentListItemsBinding.inflate(inflater, container, false)
         return binding.root
     }
