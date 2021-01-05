ca1399ffd29099d44f11bcc5e7b71eb67252ce50
cyliong <liongtheprogrammer@gmail.com>
2021-01-05 22:24:30 +0800

    Fix app crash issue after using FragmentContainerView

--- app/src/main/java/com/example/ltp/list2/ui/MainActivity.kt
+++ app/src/main/java/com/example/ltp/list2/ui/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.ltp.list2.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.ltp.list2.R
 
 class MainActivity : AppCompatActivity() {
 
-    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
