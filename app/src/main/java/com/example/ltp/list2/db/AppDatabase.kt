85ccdb4a5bec04eb1cef33040086a6113597f96c
cyliong <liongtheprogrammer@gmail.com>
2021-01-19 12:38:26 +0800

    Implement database migration logic

--- app/src/main/java/com/example/ltp/list2/db/AppDatabase.kt
+++ app/src/main/java/com/example/ltp/list2/db/AppDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [ListItem::class], version = 1)
+@Database(entities = [ListItem::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun listItemDao(): ListItemDao
@@ -14,13 +16,22 @@ abstract class AppDatabase : RoomDatabase() {
 
         @Volatile private var instance: AppDatabase? = null
 
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE list_item ADD note TEXT")
+            }
+        }
+
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
                 instance ?: Room.databaseBuilder(
                     context,
                     AppDatabase::class.java,
                     "list_database"
-                ).build().also { instance = it }
+                )
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
+                    .also { instance = it }
             }
 
     }
