75d27cad69d96cdf1a1872599a10b747805323ae
cyliong <liongtheprogrammer@gmail.com>
2021-08-22 23:08:57 +0800

    Use ComposeView in RecyclerView

--- app/src/main/java/com/example/ltp/list2/ui/ListItemsAdapter.kt
+++ app/src/main/java/com/example/ltp/list2/ui/ListItemsAdapter.kt
@@ -1,10 +1,17 @@
 package com.example.ltp.list2.ui
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.ltp.list2.databinding.RecyclerViewItemBinding
 import com.example.ltp.list2.db.ListItem
 
 class ListItemsAdapter : RecyclerView.Adapter<ListItemsAdapter.ViewHolder>() {
@@ -12,19 +19,18 @@ class ListItemsAdapter : RecyclerView.Adapter<ListItemsAdapter.ViewHolder>() {
     private var items = emptyList<ListItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ViewHolder(
-            RecyclerViewItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        ViewHolder(ComposeView(parent.context))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
     override fun getItemCount() = items.size
+
+    override fun onViewRecycled(holder: ViewHolder) {
+        super.onViewRecycled(holder)
+        holder.composeView.disposeComposition()
+    }
 
     fun setItems(items: List<ListItem>) {
         this.items = items
@@ -33,14 +39,16 @@ class ListItemsAdapter : RecyclerView.Adapter<ListItemsAdapter.ViewHolder>() {
 
     fun getItemAtPosition(position: Int) = items[position]
 
-    inner class ViewHolder(private val binding: RecyclerViewItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val composeView: ComposeView) :
+        RecyclerView.ViewHolder(composeView) {
 
         private val navController by lazy { itemView.findNavController() }
 
         private var item: ListItem? = null
 
         init {
+            composeView.setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+
             itemView.setOnClickListener {
                 item?.let {
                     navController.navigate(
@@ -52,7 +60,17 @@ class ListItemsAdapter : RecyclerView.Adapter<ListItemsAdapter.ViewHolder>() {
 
         fun bind(item: ListItem) {
             this.item = item
-            binding.textViewListItem.text = item.title
+            composeView.setContent {
+                MaterialTheme {
+                    Text(
+                        text = item.title,
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp, vertical = 10.dp)
+                    )
+                }
+            }
         }
 
     }
